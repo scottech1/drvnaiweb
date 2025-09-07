df2c1d95b25672d03cbf852c3cd29bf27bf57ce9
Deep Adhia <adhiyadeep@gmail.com>
2025-09-07 17:49:20 +0530

    changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Download, Car, Wrench, Zap, MessageCircle } from 'lucide-react'
 
 const APP_STORE_URLS = {
-  ios: 'https://itunes.apple.com/app/id6748619728?mt=8',
+  ios: 'https://apps.apple.com/us/app/drvnai/id6748619728',
   android: 'https://play.google.com/store/apps/details?id=com.sgesdevllc.drvnai'
 }
 
